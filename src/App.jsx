e3857a7e63d9c3cf00a3674c6e54b01455cb840c
Pablo Moabro <pablomoabro@gmail.com>
2024-09-27 10:14:19 -0300

    sigo trantando de desplegar en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
    
-    <BrowserRouter>
+    <BrowserRouter basename='/ProyectoFinal-Moabro-React-JS/'>
       <CartContextProvider>
         <BaseLayOut>
             <Routes>
